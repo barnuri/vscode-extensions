321c87817cf30e70084142198cedee3309d4a36a
Bar Nuri <barn@codeoasis.com>
2019-07-11 13:25:51 +0300

    version

---
+++ scripts/installMinikube.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+set -e
+
+command_exists() {
+	command -v "$@" > /dev/null 2>&1
+}
+
+installFunc() {
+	user="$(id -un 2>/dev/null || true)"
+
+	sh_c='sh -c'
+	if [ "$user" != 'root' ]; then
+		if command_exists sudo; then
+			sh_c='sudo -E sh -c'
+		elif command_exists su; then
+			sh_c='su -c'
+		else
+			cat >&2 <<-'EOF'
+			Error: this installer needs the ability to run commands as root.
+			We are unable to find either "sudo" or "su" available to make this happen.
+			EOF
+			exit 1
+		fi
+	fi
+
+    $sh_c 'apt-get update -qq >/dev/null'
+    $sh_c 'DEBIAN_FRONTEND=noninteractive apt-get upgrade -qq'
+    $sh_c 'echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list'
+    $sh_c 'DEBIAN_FRONTEND=noninteractive apt-get install -y apt-transport-https curl -y  ebtables ethtool apt-transport-https'
+    $sh_c 'curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - '
+    $sh_c 'apt-get install -y kubectl'
+    $sh_c 'apt-get update -qq >/dev/null'
+    $sh_c 'curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube'
+    $sh_c 'install minikube /usr/local/bin'
+    $sh_c 'minikube config set vm-driver none'
+    $sh_c 'minikube start'
+    
+    $sh_c 'cp /root/.kube /root/.minikube $HOME'
+    $sh_c 'chown -R $USER $HOME/.kube $HOME/.minikube'
+
+    $sh_c 'minikube addons enable ingress'
+    $sh_c 'minikube addons enable dashboard'
+    $sh_c 'minikube status'
+    $sh_c 'kubectl config view --raw --flatten --minify'
+}
+
+installFunc
--- src/installations.ts
+++ src/installations.ts
@@ -1,29 +1,11 @@
 import { getTerminal } from './extension';
-import { writeFile } from './fileHelper';
+import { writeFile, readFile, setFullPermission } from './fileHelper';
 
 export function installMinikube() {
-    const script = `
-    #!/bin/sh
-    set -e
-    sh -c echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
-    ${upgradeUbuntu()}
-    sh -c DEBIAN_FRONTEND=noninteractive apt-get install -y apt-transport-https curl -y  ebtables ethtool apt-transport-https
-    sh -c curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - 
-    sh -c DEBIAN_FRONTEND=noninteractive apt-get install -y kubectl
-    sh -c curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
-    sh -c install minikube /usr/local/bin
-    sh -c minikube config set vm-driver none
-    sh -c minikube start
-    sh -c mv /root/.kube /root/.minikube $HOME
-    sh -c chown -R $USER $HOME/.kube $HOME/.minikube
-    sh -c minikube addons enable ingress
-    sh -c minikube addons enable dashboard
-    sh -c minikube status
-    sh -c kubectl config view --raw --flatten --minify
-    `;
-
+    const script = require(`../script/installMinikube.sh`);
     writeFile('./minikubeInstall.sh', script);
-    getTerminal().sendText('wget -qO- ./minikubeInstall.sh | sh');
+    setFullPermission('./minikubeInstall.sh');
+    getTerminal().sendText('./minikubeInstall.sh');
 }
 
 export function installDocker() {
