862173034215c056f824715f16f1927d49647d2a
Bar Nuri <barn@codeoasis.com>
2019-07-11 10:20:19 +0300

    version

--- src/installations.ts
+++ src/installations.ts
@@ -6,10 +6,15 @@ export function installMinikube() {
     upgradeUbuntu(t);
     t.sendText(`
     echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list;
+    echo ----------------------------- 3
     DEBIAN_FRONTEND=noninteractive apt-get install -y apt-transport-https curl -y  ebtables ethtool apt-transport-https;
+    echo ----------------------------- 4
     curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - ;
+    echo ----------------------------- 5
     DEBIAN_FRONTEND=noninteractive apt-get install -y kubectl;
+    echo ----------------------------- 6
     curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube;
+    echo ----------------------------- 7
     install minikube /usr/local/bin;
     minikube config set vm-driver none;
     minikube start;
@@ -25,9 +30,13 @@ export function installDocker() {
     upgradeUbuntu(t);
     t.sendText(`
     add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable";
+    echo ----------------------------- 3
     DEBIAN_FRONTEND=noninteractive apt-get install apt-transport-https ca-certificates curl software-properties-common -y;
+    echo ----------------------------- 4
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -;
+    echo ----------------------------- 5
     DEBIAN_FRONTEND=noninteractive apt-get install docker-ce -y;
+    echo ----------------------------- 6
     docker -v';
     `);
 }
@@ -35,6 +44,8 @@ export function installDocker() {
 export function upgradeUbuntu(t: Terminal) {
     t.sendText(`
 DEBIAN_FRONTEND=noninteractive apt-get update -y --force-yes;
+echo ----------------------------- 1
 DEBIAN_FRONTEND=noninteractive apt-get upgrade -y --force-yes;
+echo ----------------------------- 2
     `);
 }
