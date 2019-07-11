95ef02dba5544c5c24dabcca29b594ed8087283b
Bar Nuri <barn@codeoasis.com>
2019-07-11 10:35:05 +0300

    version

--- src/installations.ts
+++ src/installations.ts
@@ -5,23 +5,23 @@ export function installMinikube() {
     const t = getTerminal();
     upgradeUbuntu(t);
     t.sendText(`
-    echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list;
+    echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list
     echo ----------------------------- 3
-    DEBIAN_FRONTEND=noninteractive apt-get install -y apt-transport-https curl -y  ebtables ethtool apt-transport-https;
+    DEBIAN_FRONTEND=noninteractive apt-get install -y apt-transport-https curl -y  ebtables ethtool apt-transport-https
     echo ----------------------------- 4
-    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - ;
+    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add - 
     echo ----------------------------- 5
-    DEBIAN_FRONTEND=noninteractive apt-get install -y kubectl;
+    DEBIAN_FRONTEND=noninteractive apt-get install -y kubectl
     echo ----------------------------- 6
-    curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube;
+    curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
     echo ----------------------------- 7
-    install minikube /usr/local/bin;
-    minikube config set vm-driver none;
-    minikube start;
-    minikube addons enable ingress;
-    minikube addons enable dashboard;
-    minikube status;
-    kubectl config view --raw --flatten --minify;
+    install minikube /usr/local/bin
+    minikube config set vm-driver none
+    minikube start
+    minikube addons enable ingress
+    minikube addons enable dashboard
+    minikube status
+    kubectl config view --raw --flatten --minify
     `);
 }
 
@@ -29,23 +29,23 @@ export function installDocker() {
     const t = getTerminal();
     upgradeUbuntu(t);
     t.sendText(`
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable";
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     echo ----------------------------- 3
-    DEBIAN_FRONTEND=noninteractive apt-get install apt-transport-https ca-certificates curl software-properties-common -y;
+    DEBIAN_FRONTEND=noninteractive apt-get install apt-transport-https ca-certificates curl software-properties-common -y
     echo ----------------------------- 4
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -;
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
     echo ----------------------------- 5
-    DEBIAN_FRONTEND=noninteractive apt-get install docker-ce -y;
+    DEBIAN_FRONTEND=noninteractive apt-get install docker-ce -y
     echo ----------------------------- 6
-    docker -v';
+    docker -v'
     `);
 }
 
 export function upgradeUbuntu(t: Terminal) {
     t.sendText(`
-DEBIAN_FRONTEND=noninteractive apt-get update -y --force-yes || true;
+DEBIAN_FRONTEND=noninteractive apt-get update -qy --force-yes  > /dev/null
 echo ----------------------------- 1
-DEBIAN_FRONTEND=noninteractive apt-get upgrade -y --force-yes || true;
+DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -qy --force-yes >> /var/log/apt/scripted-upgrades.log
 echo ----------------------------- 2
     `);
 }
