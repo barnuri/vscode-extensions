10f0c5e218cfb89ffe384098e5c0c7a48d367fbd
Bar Nuri <barn@codeoasis.com>
2019-07-11 11:52:45 +0300

    version

--- src/installations.ts
+++ src/installations.ts
@@ -1,10 +1,9 @@
 import { getTerminal } from './extension';
-import { Terminal } from 'vscode';
 
 export function installMinikube() {
     const t = getTerminal();
     t.sendText(
-        removeEmptyLines(`${upgradeUbuntu()}
+        removeEmptyLines(`
     echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list.d/kubernetes.list
     ${echoStep(2)}
     DEBIAN_FRONTEND=noninteractive apt-get install -y apt-transport-https curl -y  ebtables ethtool apt-transport-https
@@ -28,21 +27,7 @@ export function installMinikube() {
 
 export function installDocker() {
     const t = getTerminal();
-    t.sendText(
-        removeEmptyLines(
-            `${upgradeUbuntu()}
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    ${echoStep(2)}
-    DEBIAN_FRONTEND=noninteractive apt-get install apt-transport-https ca-certificates curl software-properties-common -qy
-    ${echoStep(3)}
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-    ${echoStep(4)}
-    DEBIAN_FRONTEND=noninteractive apt-get install docker-ce -qy
-    ${echoStep(5)}
-    docker -v'
-    `,
-        ).replace(/\n/g, '&&'),
-    );
+    t.sendText(`wget -qO- https://get.docker.com/ | sh`);
 }
 
 export function echoStep(num: number) {
